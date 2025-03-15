5e54521735af2af3814c0be02fb217e6b6f094e1
n3ster <nesternash2004@gmail.com>
2025-03-15 13:48:53 +0530

    added backend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import {connectDB} from "./config/connectDB.js"
 import userRouter from "./routes/user.route.js"
 import 'dotenv/config'
 import {connectCloudinary} from "./config/cloudinary.js"
-
+import adminRouter from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
 
 const app = express()
 app.use(express.json())
@@ -17,7 +18,8 @@ connectCloudinary()
 
 // Api endpoints here
 app.use("/api/user", userRouter)
-
+app.use("/api/admin", adminRouter)
+app.use("/api/doctor", doctorRouter)
 
 // Server program
 app.listen(process.env.PORT || 3000, ()=>{
