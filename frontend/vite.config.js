b08e7a32a4ca96ff6a9c3a1cd6cef92d090f4019
n3ster <nesternash2004@gmail.com>
2025-03-23 14:12:49 +0530

    added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,  // Set to a higher value (in KB)
+  },
+  rollupOptions: {
+    output: {
+      manualChunks(id) {
+        // Split vendor libraries into a separate chunk
+        if (id.includes('node_modules')) {
+          return 'vendor';
+        }
+      }
+    }
+  },
 })
